90810b835f624b15ca6f510abbf30a8ce8ca3079
Letícia Pereira <114002275+LeticiaaS2@users.noreply.github.com>
2023-09-11 23:46:42 -0300

    Update ATV2.sql

--- ATIVIDADES/ATV2.sql
+++ ATIVIDADES/ATV2.sql
@@ -9,9 +9,25 @@ Estado CHAR (2) NOT NULL ,
 Cep INT (8) CHECK (Cep>1000) NOT NULL);
 
 CREATE TABLE produtos(
-Cproduto INT (8) NOT NULL,
+Cproduto INT (8) PRIMARY KEY NOT NULL,
 Dproduto VARCHAR (50) NOT NULL,
-Perecivel BOOLEAN NOT NULL,
-Validade
-Detalhes
-Fproduto
+Perecivel BIT DEFAULT (0) NOT NULL,
+Validade DATE,
+Detalhes BLOB(1000),
+Fproduto BINARY);
+
+CREATE TABLE vendas(
+Cvenda INT (8) NOT NULL,
+Ccliente INT (8) NOT NULL,
+PRIMARY KEY (Cvenda, Ccliente),
+FOREIGN KEY (Ccliente) REFERENCES cliente (Ccliente));
+
+CREATE TABLE produto_vendas(
+Cvenda INT (8) NOT NULL,
+Ccliente INT (8) NOT NULL,
+Cproduto INT (8) NOT NULL,
+Quantidade FLOAT (13, 3) CHECK (Quantidade>0) NOT NULL,
+PRIMARY KEY (Cvenda, Ccliente, Cproduto),
+FOREIGN KEY (Ccliente) REFERENCES vendas (Ccliente),
+FOREIGN KEY (Cvenda) REFERENCES vendas (Cvenda),
+FOREIGN KEY (Cproduto) REFERENCES produtos (Cproduto));
